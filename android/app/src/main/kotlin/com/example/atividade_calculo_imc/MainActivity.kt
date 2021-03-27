8c1fbe092335be6fb48315a5a4a93e15db29d43d
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-03-27 15:33:08 -0300

    Atividade p/ dia 29-03-21 inicializada

---
+++ android/app/src/main/kotlin/com/example/atividade_calculo_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_calculo_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Atividade Calculadora IMC',
+        home: PaginaCalculadora());
+  }
+}
+
+class PaginaCalculadora extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent[400],
+      ),
+      body: CalculadoraBody(),
+    );
+  }
+}
+
+class CalculadoraBody extends StatefulWidget {
+  @override
+  _LogicaCalculadora createState() => new _LogicaCalculadora();
+}
+
+class _LogicaCalculadora extends State<CalculadoraBody> {
+  
+
+  
+  // Método p/ criação do meu Container:
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
